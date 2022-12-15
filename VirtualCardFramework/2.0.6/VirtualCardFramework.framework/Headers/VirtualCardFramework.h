fb8209d616b303262e7818126b44f36d6f5fd9b6
esekiels <e.surbakti@teravintech.com>
2022-12-15 12:15:16 +0700

    no message

---
+++ VirtualCardFramework/2.0.6/VirtualCardFramework.framework/Headers/VirtualCardFramework.h
@@ -0,0 +1,19 @@
+//
+//  VirtualCardFramework.h
+//  VirtualCardFramework
+//
+//  Created by Esekiel Surbakti on 05/12/22.
+//  Copyright © 2022 STid. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VirtualCardFramework.
+FOUNDATION_EXPORT double VirtualCardFrameworkVersionNumber;
+
+//! Project version string for VirtualCardFramework.
+FOUNDATION_EXPORT const unsigned char VirtualCardFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VirtualCardFramework/PublicHeader.h>
+
+
